548ab671872c50fdb8b5132a8e258a71b18accf3
Nikita <pitsnikita@gmail.com>
2022-04-23 20:32:28 +0300

    big commit

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <div class="app">
   <site-header></site-header>
   <main-restaurant-body class="main-restaurant-body"/>
+  <site-footer/>
   </div>
 </template>
 
 <script setup>
 import SiteHeader from "@/components/Header/SiteHeader";
 import MainRestaurantBody from "@/components/site-body/MainRestaurant/MainRestaurantBody";
+import SiteFooter from "@/components/Footer/SiteFooter";
 </script>
 
 <style>
